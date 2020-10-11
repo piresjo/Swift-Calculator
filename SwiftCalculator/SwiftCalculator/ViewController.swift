9bf08a159ed9ae5d427438b001335a48a9a9a8b9
Joe Pires <pires.joe.a@gmail.com>
2020-10-11 14:22:58 -0400

    refactor and add floating point support

--- SwiftCalculator/SwiftCalculator/ViewController.swift
+++ SwiftCalculator/SwiftCalculator/ViewController.swift
@@ -14,119 +14,114 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var calcOutput: UILabel!
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-    
-    @IBAction func divideButtonPressed(_ sender: Any) {
-        if checkEquationQueue() {
-            
-        } else {
-            mathArray.append(calcOutput.text!)
-            mathArray.append("/")
-            calcOutput.text! = "0"
-        }
-    }
-    @IBAction func multiplyButtonPressed(_ sender: Any) {
-        if checkEquationQueue() {
-            
-        } else {
-            mathArray.append(calcOutput.text!)
-            mathArray.append("*")
-            calcOutput.text! = "0"
-        }
-    }
-    @IBAction func minusButtonPressed(_ sender: Any) {
-        if checkEquationQueue() {
-            
-        } else {
-            mathArray.append(calcOutput.text!)
-            mathArray.append("-")
-            calcOutput.text! = "0"
-        }
-    }
-    @IBAction func plusButtonPressed(_ sender: Any) {
-        if checkEquationQueue() {
-            
-        } else {
-            mathArray.append(calcOutput.text!)
-            mathArray.append("+")
-            calcOutput.text! = "0"
-        }
+    @IBAction func operatorButtonPressed(_ sender: UIButton) {
+        let textVal = sender.titleLabel!.text!
+        updateBufferOperator(operatorString: textVal)
     }
     
     @IBAction func equalButtonPressed(_ sender: Any) {
         // For right now, assume [firstVal, operator, secondVal]
         if checkEquationQueue() {
             mathArray.append(calcOutput.text!)
-            print("HELLO")
-            print(calcOutput.text!)
-            print(mathArray)
-            print("HELLO")
-            let firstVal = Int(mathArray[0])
+            var firstVal : Int = 0
+            var firstValDouble : Double = 0.0
+            var secondVal : Int = 0
+            var secondValDouble : Double = 0.0
+            var firstValIsDouble = false
+            var secondValIsDouble = false
+            if mathArray[0].contains(".") || mathArray[2].contains(".") {
+                firstValDouble = Double(mathArray[0])!
+                secondValDouble = Double(mathArray[2])!
+                firstValIsDouble = true
+                secondValIsDouble = true
+            } else {
+                firstVal = Int(mathArray[0])!
+                secondVal = Int(mathArray[2])!
+            }
+            
             let operatorVal = mathArray[1]
-            let secondVal = Int(mathArray[2])
+            
             var result = 0
+            var doubleResult = 0.0
+            var displayDouble = false
             
             if operatorVal == "+" {
-                result = firstVal! + secondVal!
+                if secondValIsDouble || firstValIsDouble {
+                    doubleResult = firstValDouble + secondValDouble
+                    displayDouble = true
+                } else {
+                    result = firstVal + secondVal
+                }
             } else if operatorVal == "-" {
-                result = firstVal! - secondVal!
+                if secondValIsDouble || firstValIsDouble {
+                    doubleResult = firstValDouble - secondValDouble
+                    displayDouble = true
+                } else {
+                    result = firstVal - secondVal
+                }
             } else if operatorVal == "*" {
-                result = firstVal! * secondVal!
+                if secondValIsDouble || firstValIsDouble {
+                    doubleResult = firstValDouble * secondValDouble
+                    displayDouble = true
+                } else {
+                    result = firstVal * secondVal
+                }
             } else {
-                result = firstVal! / secondVal!
+                if secondValIsDouble || firstValIsDouble || (!(firstValIsDouble) && !(secondValIsDouble) && firstVal % secondVal != 0) {
+                    if firstValIsDouble || secondValIsDouble {
+                        doubleResult = firstValDouble / secondValDouble
+                        displayDouble = true
+                    } else {
+                        doubleResult = Double(firstVal) / Double(secondVal)
+                        displayDouble = true
+                    }
+                    
+                } else {
+                    result = firstVal / secondVal
+                }
             }
             
             mathArray = []
-            calcOutput.text = String(result)
-            //print("MOO")
-            //print(mathArray)
-            //print("MOO")
+            if displayDouble {
+                calcOutput.text = String(doubleResult)
+            } else {
+                calcOutput.text = String(result)
+            }
+            
         }
     }
     
-    @IBAction func zeroButtonPressed(_ sender: Any) {
-        updateBuffer(updateString: "0")
-    }
-    @IBAction func oneButtonPressed(_ sender: Any) {
-        updateBuffer(updateString: "1")
-    }
     
-    @IBAction func twoButtonPressed(_ sender: Any) {
-        updateBuffer(updateString: "2")
-    }
-    
-    @IBAction func threeButtonPressed(_ sender: Any) {
-        updateBuffer(updateString: "3")
-    }
-    
-    @IBAction func fourButtonPressed(_ sender: Any) {
-        updateBuffer(updateString: "4")
-    }
-    
-    @IBAction func fiveButtonPressed(_ sender: Any) {
-        updateBuffer(updateString: "5")
-    }
-    
-    @IBAction func sixButtonPressed(_ sender: Any) {
-        updateBuffer(updateString: "6")
-    }
-    
-    @IBAction func sevenButtonPressed(_ sender: Any) {
-        updateBuffer(updateString: "7")
-    }
-    
-    @IBAction func eightButtonPressed(_ sender: Any) {
-        updateBuffer(updateString: "8")
+    @IBAction func numberButtonPressed(_ sender: UIButton) {
+        let textVal = sender.titleLabel!.text!
+        updateBuffer(updateString: textVal)
     }
     
     
-    @IBAction func nineButtonPressed(_ sender: Any) {
-        updateBuffer(updateString: "9")
+    @IBAction func greyButtonPressed(_ sender: UIButton) {
+        let textVal = (sender as AnyObject).titleLabel!.text!
+        if textVal == "%" {
+            
+        } else if textVal == "+/-" {
+            var numberVal = Int(calcOutput.text!)!
+            numberVal *= -1
+            calcOutput.text = String(numberVal)
+        } else {
+            mathArray = []
+            calcOutput.text = "0"
+        }
+    }
+    
+    
+    @IBAction func dotButtonPressed(_ sender: UIButton) {
+        if !(calcOutput.text!.contains(".")) {
+            calcOutput.text! += "."
+        }
     }
     
     func updateBuffer(updateString : String) {
@@ -137,11 +132,23 @@ class ViewController: UIViewController {
         }
     }
     
+    func updateBufferOperator(operatorString : String) {
+        if checkEquationQueue() {
+            
+        } else {
+            mathArray.append(calcOutput.text!)
+            mathArray.append(operatorString)
+            calcOutput.text! = "0"
+        }
+    }
+    
     func checkEquationQueue() -> Bool {
         if mathArray.contains("+") || mathArray.contains("-") || mathArray.contains("*") || mathArray.contains("/") {
             return true
         }
         return false
     }
+    
+    
 }
 
